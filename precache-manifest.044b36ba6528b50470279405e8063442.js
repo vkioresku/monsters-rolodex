2d816845f89ef46bb519c8c7c08dc028428ffd89
vkioresku <>
2019-07-09 14:57:26 +0300

    Updates

---
+++ precache-manifest.044b36ba6528b50470279405e8063442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e0da393ca785400ece49d52a3bc40d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "50d48684000c4863b57e",
+    "url": "/monsters-rolodex/static/css/main.0c5b0a47.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "50d48684000c4863b57e",
+    "url": "/monsters-rolodex/static/js/main.f94542ce.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
